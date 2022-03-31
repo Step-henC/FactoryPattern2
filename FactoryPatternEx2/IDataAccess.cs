e1da645d42ed47e686824622ac1168c590517363
Stephen Cunningham <stevecunningham@outlook.com>
2022-03-30 20:22:53 -0500

    initial commit

---
+++ FactoryPatternEx2/IDataAccess.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternEx2
+{
+    public interface IDataAccess
+    {
+
+        List<Product> LoadData();
+        void SaveData();
+    }
+    public class DataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product() { Name = "Jordans", Price = 500},
+            new Product() { Name = "Lamp", Price = 200},
+            new Product() { Name ="Ceiling fan", Price = 100},
+        };
+        
+        public new List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from DataAccess");
+            return Products;
+        }
+        public new void SaveData()
+        {
+            Console.WriteLine("I am saving data to the DataBase");
+        }
+    }
+    public class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> SqlProd  = new List<Product>()
+        {
+            new Product() { Name = "Microphone", Price = 342},
+            new Product() { Name = "greasepan", Price = 1},
+            new Product() { Name ="Tesla Car", Price = 40},
+        };
+        public new List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from SQL");
+            return SqlProd;
+        }
+        public new void SaveData()
+        {
+            Console.WriteLine("I am saving data to the SQL database");
+
+        }
+    }
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> MgoProd = new List<Product>()
+        {
+            new Product() { Name = "Cat toy", Price = 65},
+            new Product() { Name = "wrench", Price = 12},
+            new Product() { Name ="piano key", Price = 22},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from Mongo");
+            return MgoProd;
+        }
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data to the Mongo Database");
+        }
+
+    }
+    public static class DataFactory 
+    {
+        public static IDataAccess GetDataAccessType(string DataBaseType)
+        {
+            switch (DataBaseType.ToLower())
+            {
+                case "sql":
+                    return new SQLDataAccess();
+                case "list":
+                    return new DataAccess();
+                case "mongo":
+                    return new MongoDataAccess();
+                default:
+                    return new DataAccess();
+            }
+        }
+    }
+}
---
+++ FactoryPatternEx2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternEx2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /* In one solution, you will need to create some new types.
+            A class called Product with 2 properties, Name and Price
+            An interface called IDataAccess with 2 methods, LoadData() and SaveData()
+            3 classes that implement the interface and its members (the methods), ListDataAccess, SQLDataAccess, and MongoDataAccess
+            Last you will need to create a class called DataAccessFactory
+
+
+            The 3 classes that implement your IDataAccess interface should provide implementations for the 2 methods 
+            LoadData(): Just print to the console 
+            “I am reading data from {whatever type class this is, i.e: list, SQL database, mongo database}” 
+            and then return a new list of Product.
+            SaveData(): Print “I am saving data to a {whatever type} database” to the console
+
+
+            Once those 3 classes have the methods implemented, you will need to fill out your factory
+            This shall be a public static class
+            There will be one method with the following signature: 	
+            public static IDataAccess GetDataAccessType(string databaseType)
+
+            Switch on the databaseType variable
+            If a user types in “list” this should return a new instance of the ListDataAccess
+            If a user types in “sql” this should return a new instance of the SQLDataAccess
+            If a user types in “mongo” this should return a new instance of the MongoDataAccess
+
+
+Lastly in the Main()
+Ask the user what database they want to use: a List, SQL, or Mongo
+Store their response in a variable
+Call your factory method and pass in what the user types and store what your factory method returns in an IDataAccess variable
+Call the LoadData and SaveData methods of the IDataAccess variable
+Run the program a few times using the different types
+*/
+            Console.WriteLine("Hi! What database do you want to use?");
+            Console.WriteLine("List, SQL, or Mongo");
+            
+
+            string DataAnswer = Console.ReadLine().ToLower();
+            
+            
+
+
+
+
+
+            IDataAccess dataType = DataFactory.GetDataAccessType(DataAnswer);
+            var products = dataType.LoadData();
+            dataType.SaveData();
+
+
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Price is {product.Price} for {product.Name}");
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+            
+        
+    }
+}
